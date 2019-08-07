8a3103d909419ea3d565a6c1b891d21a664e628d
李祥宇 <972890694@qq.com>
2019-08-07 11:36:09 +0800

    conmit

--- src/main.js
+++ src/main.js
@@ -8,11 +8,7 @@ import 'element-ui/lib/theme-chalk/index.css';
 import './assets/css/base.css'
 
 import crumbs from './components/index/crumbs.vue'
-import page from './components/index/page.vue'
-import grid from './components/index/grid.vue'
 Vue.component('crumbs',crumbs)
-Vue.component('page',page)
-Vue.component('grid',grid)
 
 Vue.config.productionTip = false
 
